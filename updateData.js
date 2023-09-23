0391d2efd88a6e951d44823b431a01f67a09cd63
tapastrades <tapas.trades@gmail.com>
2023-09-23 18:31:21 +0530

    Initial commit on new branch

--- updateData.js
+++ updateData.js
@@ -4,7 +4,7 @@ var openPositions = [
     { symbol: 'GOOGL', openPrice: 2700.00, percentChange: -1.85, allocation: 13, strategy: 'MomentumSystemic', color: 'rgba(243, 203, 75, 0.8)' },
     // Add more positions as needed
 ];
-function updateOpenPositions() {
+function updateOpenPositions(openPositions) {
     var openPositionsTable = document.getElementById('open-positions').getElementsByTagName('tbody')[0];
 
     // Clear the existing table rows
@@ -62,6 +62,13 @@ function updateOpenPositions() {
         data: freeMarginPercentage,
         backgroundColor: 'rgba(136, 136, 142, 0.56)',
     });
+
+    var existingChart = Chart.getChart("allocation-pie-chart");
+
+    // Check if the chart exists and destroy it
+    if (existingChart) {
+      existingChart.destroy();
+    }
 
     // Create a pie chart using Chart.js
     var ctx = document.getElementById('allocation-pie-chart').getContext('2d');
@@ -149,7 +156,15 @@ function getRandomColor() {
     return color;
 }
 
-function getChartData() {
+function getChartData(stdBalance,stdNifty,date) {
+
+    // Assuming you have an existing chart with the ID 'myChart'
+    var existingChart = Chart.getChart("myChart");
+
+    // Check if the chart exists and destroy it
+    if (existingChart) {
+      existingChart.destroy();
+    }
 
     // Get the chart canvas element
     var ctx = document.getElementById('myChart').getContext('2d');
@@ -162,8 +177,8 @@ function getChartData() {
     gradient.addColorStop(1, 'rgba(84, 76, 244, 0)'); // En
 
     var dataset1 = {
-            label: 'Sample Data',
-            data: [12, 19, 3, 5, 2, 3, 2, 10, 15, 18, 20],
+            label: 'Nifty 50',
+            data: stdNifty,
             borderColor: 'blue', // Set the line color to blue
             pointBackgroundColor: 'blue', // Set the data point dot color to blue
             fill: true, // Disable fill under the line (optional)
@@ -181,8 +196,8 @@ function getChartData() {
     gradient.addColorStop(1, 'rgba(243, 203, 75, 0)'); // En
 
     var dataset2 = {
-            label: 'Sample Data1',
-            data: [9, 5, 8, 10, 5, 12, 9, 13, 8, 7, 9],
+            label: 'Strategy Performance',
+            data: stdBalance,
             borderColor: 'rgba(243, 203, 75, 0.8)', // Set the line color to blue
             pointBackgroundColor: 'rgba(243, 203, 75, 0.8)', // Set the data point dot color to blue
             backgroundColor: gradient,
@@ -193,7 +208,7 @@ function getChartData() {
 
     // Sample data for the chart (replace with your data)
     var chartData = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Orange', 'Orange', 'Orange','Orange', 'Orange' ],
+        labels: date,
         datasets: [dataset1, dataset2]
     };
 
@@ -241,93 +256,154 @@ function getChartData() {
     myChart.update();   
 }
 // Example function to calculate the sum value (replace this with your own logic)
-function calculateSumValue() {
+function calculateSumValue(c, p) {
     // Replace with your logic to calculate the sum value
-    const sumValue = 1234.56;
+    const sumValue = c - p;
     return "₹ " + sumValue.toFixed(2); // Format the sum value as needed
 }
 
-
-// Function to read a text file and parse its content into a dictionary
-function readTextFileAndCreateDictionary(filePath) {
-  try {
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const lines = fileContent.split('\n');
-
-    const dictionary = {};
-
-    lines.forEach((line) => {
-      const [key, value] = line.split(':').map((item) => item.trim());
-      if (key && value) {
-        dictionary[key] = value;
-      }
-    });
-
-    return dictionary;
-  } catch (error) {
-    console.error('Error reading the file:', error.message);
-    return {};
-  }
+function convertK(inputString){
+    // Remove the "k" character
+    const stringWithoutK = inputString.replace("k", "");
+    // console.log(stringWithoutK)
+    // Convert the string to an integer
+    const integerValue = parseFloat(stringWithoutK, 10);
+    return integerValue*1000
 }
-
 // Function to update the dynamic text
 function updateDynamicText() {
-    // const fs = require('fs');
+    
+    const getList = async () => {
+        return new Promise((resolve, reject) => {
+            fetch('balanceCurve.txt')
+              .then(response => response.text())
+              .then(data => {
+                // Process the data from the file
+                const lines = data.split("\n");
+                const list = lines;
+                // console.log(list);
+                resolve(list);
+              })
+              .catch(error => {
+                console.error('Error reading the file:', error);
+                reject(error); // Reject the promise if an error occurs
+              });
+          });
+        };
+    var dataList = [];    
+    // Usage example:
+    getList()
+      .then(list => {
+        
+        var itemsToTake = 1
+        // Get the selected time period from the dropdown
+        const selectedTimePeriodElem = document.getElementById("time-period-select");
+        const selectedTimePeriod = selectedTimePeriodElem.value
+        if (selectedTimePeriod == "this-week"){
+            selectedTimePeriodElem.style.width = "170px";
+            // Calculate the number of items to take (minimum of 7 or the object's length)
+            itemsToTake = Math.min(7, list.length);
+        }
+        else if (selectedTimePeriod == "this-month"){
+            selectedTimePeriodElem.style.width = "190px";
+            itemsToTake = Math.min(30, list.length);
+        }
+        else if (selectedTimePeriod == "3-months"){
+            selectedTimePeriodElem.style.width = "200px";
+            itemsToTake = Math.min(90, list.length);
+        }
+        else if (selectedTimePeriod == "6-months"){
+            selectedTimePeriodElem.style.width = "205px";
+            itemsToTake = Math.min(180, list.length);
+        }
+        else if (selectedTimePeriod == "this-year"){
+            selectedTimePeriodElem.style.width = "160px";
+            itemsToTake = Math.min(365, list.length);
+        }
+        
+        // Select the last itemsToTake items from the array
+        const selectedItems = list.slice(-itemsToTake);
+        // Convert the selected items back into an object
+        // const selectedObject = Object.fromEntries(selectedItems);
+        currentBalance = convertK(selectedItems.slice(-1)[0].split(',')[1]);
+        previousBalance = convertK(selectedItems.slice(0)[0].split(',')[1]);
+        var seperated = getSeperated(selectedItems);
+        var stdBalance = standardizeToZeroOne(seperated[0]);
+        var stdNifty = standardizeToZeroOne(seperated[1]);
+        console.log(stdBalance)
+        console.log(stdNifty)
+        // Handle the list here
+        const percentChange = ((currentBalance - previousBalance) / previousBalance * 100).toFixed(2);
+        // Set the change status based on the data (you can implement your own logic)
+        const changeStatus = percentChange >= 0 ? "up" : "down";
 
+        // Update the dynamic text
+        const changeDircElem = document.getElementById("change-status");
+        const percentChangeElem = document.getElementById("percent-change");
+        const sumValueElement = document.getElementById("sum-value"); // Get the sum-value element
+        const sumBoxElem = document.querySelector(".sum-box");
+        if (changeStatus == "up"){
+            changeDircElem.style.color = "#86d18c";
+            changeDircElem.textContent = changeStatus;
+            percentChangeElem.style.color = "#86d18c";
+            sumBoxElem.style.background = "#86d18c";
+        }
+        else if (changeStatus == "down"){
+            changeDircElem.style.color = "#eb5e6a";
+            changeDircElem.textContent = changeStatus;
+            percentChangeElem.style.color = "#eb5e6a";
+            sumBoxElem.style.background = "#eb5e6a";
+        }
+        percentChangeElem.textContent = `${Math.abs(percentChange)}%`;
+
+        // Set the value for sum-value
+        const sumValue = calculateSumValue(currentBalance, previousBalance); // Replace with your logic to calculate the sum value
+        sumValueElement.textContent = sumValue; // Set the sum value text
+
+        getChartData(stdBalance, stdNifty, seperated[2]);
+        
+      })
+      .catch(error => {
+        // Handle errors here
+        console.error('Error:', error);
+      });
     // Replace these values with actual data from your source
     const currentPrice = 100; // Replace with the current price
     const previousPrice = 97.7; // Replace with the previous price
 
-    const percentChange = ((currentPrice - previousPrice) / previousPrice * 100).toFixed(2);
+    fetch('openPosition.json')
+      .then(response => response.json())
+      .then(data => {
+        // 'data' will contain the loaded array
+        updateOpenPositions(data);
+                
+      })
+      .catch(error => {
+        console.error('Error loading data:', error);
+      });
 
-    // Get the selected time period from the dropdown
-    const selectedTimePeriodElem = document.getElementById("time-period-select");
-    const selectedTimePeriod = selectedTimePeriodElem.value
-    if (selectedTimePeriod == "this-week"){
-        selectedTimePeriodElem.style.width = "170px";
-    }
-    else if (selectedTimePeriod == "this-month"){
-        selectedTimePeriodElem.style.width = "190px";
-    }
-    else if (selectedTimePeriod == "3-months"){
-        selectedTimePeriodElem.style.width = "200px";
-    }
-    else if (selectedTimePeriod == "6-months"){
-        selectedTimePeriodElem.style.width = "205px";
-    }
-    else if (selectedTimePeriod == "this-year"){
-        selectedTimePeriodElem.style.width = "160px";
-    }
-
-    // Set the change status based on the data (you can implement your own logic)
-    const changeStatus = percentChange >= 0 ? "up" : "down";
-
-    // Update the dynamic text
-    const changeDircElem = document.getElementById("change-status");
-    const percentChangeElem = document.getElementById("percent-change");
-    const sumValueElement = document.getElementById("sum-value"); // Get the sum-value element
-    const sumBoxElem = document.querySelector(".sum-box");
-    if (changeStatus == "up"){
-        changeDircElem.style.color = "#86d18c";
-        changeDircElem.textContent = changeStatus;
-        percentChangeElem.style.color = "#86d18c";
-        sumBoxElem.style.background = "#86d18c";
-    }
-    else if (changeStatus == "down"){
-        changeDircElem.style.color = "#eb5e6a";
-        changeDircElem.textContent = changeStatus;
-        percentChangeElem.style.color = "#eb5e6a";
-        sumBoxElem.style.background = "#eb5e6a";
-    }
-    percentChangeElem.textContent = `${Math.abs(percentChange)}%`;
-
-    // Set the value for sum-value
-    const sumValue = calculateSumValue(); // Replace with your logic to calculate the sum value
-    sumValueElement.textContent = sumValue; // Set the sum value text
-
-    getChartData();
-    updateOpenPositions();
 }
+
+function standardizeToZeroOne(data) {
+  const min = Math.min(...data);
+  const max = Math.max(...data);
+
+  return data.map(value => (value - min) / (max - min));
+}
+
+function getSeperated(list){
+    var balance = [];
+    var nifty = [];
+    var date = [];
+    for (const item of list){
+        i = item.split(',');
+        balance.push(convertK(i[1]));
+        nifty.push(i[2]);
+        date.push(i[0])
+    }
+    return [balance, nifty, date]
+}
+
 
 
 
@@ -335,16 +411,6 @@ function updateDynamicText() {
 document.getElementById("time-period-select").addEventListener("change", updateDynamicText);
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('balanceCurve.txt')
-      .then(response => response.text())
-      .then(data => {
-        // Process the data from the file
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('Error reading the file:', error);
-      });
-
     // Call the function to update the text and chart when the DOM is ready
     updateDynamicText();
 });
