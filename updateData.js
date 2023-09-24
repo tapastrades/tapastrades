59ae44e70fbe7e630211d7155a893bf7299ce2f3
tapastrades <tapas.trades@gmail.com>
2023-09-24 11:45:55 +0530

    Initial commit on new branch

--- updateData.js
+++ updateData.js
@@ -412,6 +412,23 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Add an event listener for the "resize" event
-window.addEventListener('resize', updateDynamicText);
+window.addEventListener('resize', function(){
+    // Reload the page when the window is resized
+    window.location.reload();
+});
 
 
+const sidebarButton = document.querySelector('.sidebar-button');
+const sidebar = document.querySelector('.sidebar');
+const mainContent = document.querySelector('.main-content');
+
+// Function to toggle the sidebar's display property
+function toggleSidebarDisplay() {
+    if (sidebar.style.display === 'flex') {
+        sidebar.style.display = 'none'; // Hide the sidebar
+    } else {
+        sidebar.style.display = 'flex'; // Show the sidebar
+    }
+}
+// Add a click event listener to the button
+sidebarButton.addEventListener('click', toggleSidebarDisplay);
